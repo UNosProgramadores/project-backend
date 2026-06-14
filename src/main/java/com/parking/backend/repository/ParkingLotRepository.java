fcbd36eb5cc0443fc1d7d221e432d4cb09705e12
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-06-14 18:30:33 -0500

    feature: add ParkingLotRepository.java

---
+++ src/main/java/com/parking/backend/repository/ParkingLotRepository.java
@@ -0,0 +1,9 @@
+package com.parking.backend.repository;
+
+import com.parking.backend.entity.ParkingLot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
+}
