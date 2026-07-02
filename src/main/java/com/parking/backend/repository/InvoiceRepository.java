249de0cf21de3c2ab795d0d82279002ce3765b54
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-01 19:11:46 -0500

    feat: add DiscountConfig, Invoice, Payment and VehicleType repositories

---
+++ src/main/java/com/parking/backend/repository/InvoiceRepository.java
@@ -0,0 +1,9 @@
+package com.parking.backend.repository;
+
+import com.parking.backend.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+}
---
+++ src/main/java/com/parking/backend/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.parking.backend.repository;
+
+import com.parking.backend.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
