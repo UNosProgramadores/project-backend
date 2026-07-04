27a146597f952bfcf257e8efa8ba282699ce9e93
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-04 15:36:03 -0500

    feat: add new necessary DTO's

---
+++ src/main/java/com/parking/backend/dto/ClaimVehicleRequest.java
@@ -0,0 +1,13 @@
+package com.parking.backend.dto;
+
+public class ClaimVehicleRequest {
+
+    private String plate;
+    private String bikeRegistration;
+
+    public String getPlate() { return plate; }
+    public void setPlate(String plate) { this.plate = plate; }
+
+    public String getBikeRegistration() { return bikeRegistration; }
+    public void setBikeRegistration(String bikeRegistration) { this.bikeRegistration = bikeRegistration; }
+}
---
+++ src/main/java/com/parking/backend/dto/StaffRequest.java
@@ -0,0 +1,42 @@
+package com.parking.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class StaffRequest {
+
+    @NotBlank(message = "Document is required")
+    private String document;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    private String phone;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotNull(message = "Parking lot ID is required")
+    private Long parkingLotId;
+
+    public String getDocument() { return document; }
+    public void setDocument(String document) { this.document = document; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public Long getParkingLotId() { return parkingLotId; }
+    public void setParkingLotId(Long parkingLotId) { this.parkingLotId = parkingLotId; }
+}
