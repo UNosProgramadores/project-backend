0099a0849f6baf7bfbb53820afa12b03d79c517d
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-07 17:25:21 -0500

    feat: add query to the repository

--- src/main/java/com/parking/backend/repository/InvoiceRepository.java
+++ src/main/java/com/parking/backend/repository/InvoiceRepository.java
@@ -2,8 +2,15 @@ package com.parking.backend.repository;
 
 import com.parking.backend.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    @Query("SELECT i FROM Invoice i JOIN i.payment p JOIN p.entryRecord er JOIN er.cell c JOIN c.parkingLot pl WHERE pl.id = :parkingLotId")
+    List<Invoice> findByParkingLotId(@Param("parkingLotId") Long parkingLotId);
 }
